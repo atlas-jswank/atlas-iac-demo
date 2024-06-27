56421157767bf614242463968a68adf116ef4b5e
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-06-27 11:14:05 -0500

    Add azure

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as synced_folder from "@pulumi/synced-folder";
+import * as azure from "@pulumi/azure-native";
 
 // Import the program's configuration settings.
 const config = new pulumi.Config();
@@ -104,3 +105,40 @@ export const originURL = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
 export const originHostname = bucket.websiteEndpoint;
 // export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
 // export const cdnHostname = cdn.domainName;
+
+// Create a resource group for the website.
+const resourceGroup = new azure.resources.ResourceGroup("resource-group", {
+  location: "WestUS",
+});
+
+// Create a blob storage account.
+const account = new azure.storage.StorageAccount("account", {
+  resourceGroupName: resourceGroup.name,
+  kind: "StorageV2",
+  sku: {
+    name: "Standard_LRS",
+  },
+});
+
+// Configure the storage account as a website.
+const website = new azure.storage.StorageAccountStaticWebsite("website", {
+  resourceGroupName: resourceGroup.name,
+  accountName: account.name,
+  indexDocument: indexDocument,
+  error404Document: errorDocument,
+});
+
+// Use a synced folder to manage the files of the website.
+const syncedFolder = new synced_folder.AzureBlobFolder("synced-folder", {
+  path: path,
+  resourceGroupName: resourceGroup.name,
+  storageAccountName: account.name,
+  containerName: website.containerName,
+});
+
+export const azureUrl = account.primaryEndpoints.apply(
+  (endpoints) => endpoints.web
+);
+export const azureHostname = account.primaryEndpoints.apply(
+  (endpoints) => new URL(endpoints.web)
+).hostname;
